b1b7e92995361071a4f48f40304cd72b9eb6d71d
Nastya-Mart <nastya.mart749@gmail.com>
2024-10-07 20:48:05 +0300

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,20 @@
+# "1st program"
+print((9 ** 0.5) * 5)
+
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3rd program"
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# "4th program"
+
+print (float("123.456"))
+
+print (123.456 * 10)
+
+print(1234.56 // 4)
+
+print(1234.56 // 308.0)
+
+print(int(4.0))
